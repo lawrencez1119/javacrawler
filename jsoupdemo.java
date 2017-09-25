85e343e9045cb5c6200f4ad9d76c57449c7709e8
lawrencez1119 <lawrencez1118@sina.com>
2017-09-26 00:45:37 +0800

    Update jsoupdemo.java

--- jsoupdemo.java
+++ jsoupdemo.java
@@ -7,6 +7,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+/*copyright @ lawrence Zheng*/
 
 public class jsoupdemo {
 	public static void main(String[] args) throws IOException{
